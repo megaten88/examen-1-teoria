9900f4e645053e145e3a2f368e184b15ce7938a9
Nahin Buezo <nahinbuezo@gmail.com>
2021-05-20 15:39:02 -0600

    lectura de texto
    lectura de input y archivo de texto.

--- src/examen1/Examen1.java
+++ src/examen1/Examen1.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package examen1;
-
+import java.util.Scanner;
+import java.io.*;
 /**
  *
  * @author Alejandro Valladares, Mayra Salazar, Nahin Buezo
@@ -16,6 +17,40 @@ public class Examen1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        String texto;
+        String texto2="";
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            System.out.println("Ingrese el texto: ");
+            texto = teclado.nextLine();
+            try {
+                archivo = new File("C:\\texto.txt");
+                fr = new FileReader(archivo);
+                br = new BufferedReader(fr);
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    texto2+=linea;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (null != fr) {
+                        fr.close();
+                    }
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+             System.out.println(texto);
+              System.out.println(texto2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
